21c5ae819466658650fde7f7457d56843bff8bbb
elmstreetvictim <37093312+elmstreetvictim@users.noreply.github.com>
2021-05-03 18:24:19 -0500

    Update and rename SabnzbdPushbullet.py to SabnzbdLunaseaNotify.py

--- SabnzbdLunaseaNotify.py
+++ SabnzbdLunaseaNotify.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+# Enjoy! <3 ElmStreetVictim
+
 import os
 import sys
-import httplib
-import urllib
+import http.client
+import urllib.request, urllib.parse, urllib.error
 import sys
 import requests
 import json
-
-try:
-    (scriptname, notification_type, notification_title, notification_text, parameters) = sys.argv
-except:
-    print "No commandline parameters found"
-    sys.exit(1)
 
 ## argv[1]:
 ## Type of notification
@@ -33,10 +30,8 @@ except:
 ## argv[3]: Job name
 
 def main(argv):
-
-    # These are UTF-8 encoded emojis. In line 2 of this file the UTF-8 
-    # encoding is declared.
-    
+    #argv[0] is the script name
+    #argv[1] is the type, shown above
     notification_type = argv[1]
     title = ''
     if notification_type == 'complete':
@@ -61,24 +56,28 @@ def main(argv):
         title = '[⛔️]'
     elif notification_type == 'warning':
         title = '[⚠️]'
-    else: 
+    else:
+        #failsafe to create a semi-pretty title
         title = '[' + argv[2] + ']'
+        
+    #argv[3] is the message payload sent by Sabnzbd
     notification_body = argv[3]
-    pushbullet(title,notification_body)
+    LunaseaNotify(title,notification_body)
     sys.exit(0)
 
-def pushbullet(title,body):
+def LunaseaNotify(title,body):
+    # iOS will only display the message title on the lock screen
+    # to see the message body on the lock screen we need to put the
+    # title and body together and send that string as the "title" of the message.
+    #similar to : "[✅]Download XYZ Completed"
 
-    # Pushbullet notifications on iOS only display the message title on the lock screen
-    # to see the message body on the lock screen we need to put the body and title together
-    # and send that string as the title of the message.
-
-    appToken='YOUR_API_TOKEN_HERE'
+    device_token="YOUR TOKEN HERE (Leave the quotation marks!)"
+    base_url = 'https://notify.lunasea.app/v1/custom/device/'
+    full_url = base_url + device_token
     lock_screen_message = title + body
-    postHeaders = {"Access-Token":appToken,"Content-Type":"application/json"}
-    body = {"type":"note","title":lock_screen_message,"body":body,"Content-Type": "application/json"}
-    r = requests.post('https://api.pushbullet.com/v2/pushes',data=json.dumps(body),headers=postHeaders)
-
+    json_body = {"title":"Sabnzbd Message","body":lock_screen_message}
+    r = requests.post(full_url, json = json_body)
+    
 
 if __name__ == '__main__':
     main(sys.argv)
